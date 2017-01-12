bf9409e914c8c7e91c783ce639fa622049d1cd01
Rob <Robert.scott@cyfoethnaturiolcymru.gov.uk>
2017-01-12 09:48:56 +0000

    first Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var SlackBot = require("slackbots");
+var request = require("request");
+
+var bot = new SlackBot({
+	token: "xoxb-125411685440-kJEZ13xp2liqVR2zYPHCY5tX",
+	name: "snow_reporter"
+});
+
+bot.on("start", function () {
+
+	var params = {
+        icon_emoji: ':snowman:'
+    };
+
+    request("http://www.myweather2.com/developer/weather.ashx?uac=J8AGqmQdGv&uref=697ed4ad-377c-4122-84af-4fa0ddae8dac&output=json", function (err, res, body) {
+		if (err)
+			console.log(err);
+
+		var report = JSON.parse(body);
+
+		var snowReport = report.weather.snow_report[0];
+
+		var message = "";
+
+		Object.keys(snowReport).forEach(function(key) {
+		    message += key + " : " + snowReport[key] + "\n";
+		});
+
+		console.log(message);
+
+
+		bot.postMessageToChannel("general", message, params);
+	});
+});
+
