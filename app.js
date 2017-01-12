fc19605cfcc5b8d7033f1c99e7fc0046c7335f89
Rob <rgs24893@gmail.com>
2017-01-12 13:19:25 +0000

    poll report and post if report is newer

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var SlackBot = require("slackbots");
 var request = require("request");
+var Forecast = require("./models");
 
 var config = require("./config");
 
@@ -8,30 +9,45 @@ var bot = new SlackBot({
 	name: "snow_reporter"
 });
 
+var forecast = null;
+
 bot.on("start", function () {
 
 	var params = {
         icon_emoji: ':snowman:'
     };
 
-    request("http://www.myweather2.com/developer/weather.ashx?uac=J8AGqmQdGv&uref=697ed4ad-377c-4122-84af-4fa0ddae8dac&output=json", function (err, res, body) {
+    setInterval(GetReport, 60000);
+
+ //    request("http://www.myweather2.com/developer/weather.ashx?uac=J8AGqmQdGv&uref=697ed4ad-377c-4122-84af-4fa0ddae8dac&output=json", function (err, res, body) {
+	// 	if (err)
+	// 		console.log(err);
+
+	// 	var report = JSON.parse(body);
+
+	// 	forecast = new Forecast(report.weather.snow_report[0]);
+
+	// 	bot.postMessageToChannel("general", message, params);
+
+	// });
+});
+
+function GetReport () {
+	request("http://www.myweather2.com/developer/weather.ashx?uac=J8AGqmQdGv&uref=697ed4ad-377c-4122-84af-4fa0ddae8dac&output=json", function (err, res, body) {
 		if (err)
 			console.log(err);
 
 		var report = JSON.parse(body);
 
-		var snowReport = report.weather.snow_report[0];
-
-		var message = "";
-
-		Object.keys(snowReport).forEach(function(key) {
-		    message += key + " : " + snowReport[key] + "\n";
-		});
-
-		console.log(message);
-
-
-		bot.postMessageToChannel("general", message, params);
+		if (forecast === null) {
+			forecast = new Forecast(report.weather.snow_report[0]);
+			console.log(forecast.print());
+			// bot.postMessageToChannel("general", forecast.print, params);
+		} else {
+			if (forecast.compare(report.weather.snow_report[0])) {
+				forecast = new Forecast(report.weather.snow_report[0]);
+				console.log(forecast.print);
+			}
+		}
 	});
-});
-
+}
---
+++ models.js
@@ -0,0 +1,25 @@
+var Forecast = function (forecast) {
+	this.conditions = forecast.conditions;
+	this.last_snow_date = forecast.last_snow_date;
+	this.lower_snow_depth = forecast.lower_snow_depth;
+	this.report_date = new Date(forecast.report_date);
+	this.upper_snow_depth = forecast.upper_snow_depth;
+
+	this.print = function () {
+		return "Report Date : " + this.report_date + "\n" +
+		"Current Conditions : " + this.conditions + "\n" +
+		"Lower Snow Depth 	: " + this.lower_snow_depth + "cm\n" +
+		"Upper Snow Depth 	: " + this.upper_snow_depth + "cm\n" +
+		"Last Snow Date 	: " + this.last_snow_date;
+	};
+
+	this.compare = function (forecast) {
+		var newDate = new Date(forecast.report_date);
+		if (newDate.getTime() > this.report_date.getTime()){
+			return true;
+		}
+		return false;
+	};
+};
+
+module.exports = Forecast;
