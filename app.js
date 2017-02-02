361cd8e1a51e373639168f108b08d1c0cbe2d754
Matthew Uzzell <matthew.uzzell@osirium.com>
2017-02-02 17:33:02 +0000

    Tweaked timings for hypeposting

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var config = config.readConfig(process.env.CONFIG);
 
 var bot = new Bot(config);
 
-bot.registerJob("MorningCheck", "0 45 9 * * 1-5", () => bot.postPics("Soon", null));
-bot.registerJob("LunchCheck", "0 0 13 * * 1-5", () => bot.postPics("Soon", null));
-bot.registerJob("EveningCheck", "0 30 15 * * 1-5", () => bot.postPics("Soon", null));
+bot.registerJob("MorningCheck", "0 45 9 3 2 1-5", () => bot.postPics("THE 5:30 HYPE SERVICE TO FUNSVILLE WILL ARRIVE ON PLATFORM MEMES", null));
+bot.registerJob("HourlyPost",   "0 0 10-16 3 2 1-5", () => bot.postPics("Your hourly reminder you are still at work, and not here:", null));
+bot.registerJob("LunchCheck",   "0 0 17 3 2 1-5", () => bot.postPics("LAST CALL FOR THE 5:30 HYPE SERVER TO FUNSVILLE ON PLATFORM MEMES", null));
+bot.registerJob("EveningCheck", "0 30 17 3 2 1-5", () => bot.postPics("ITS OVER! ITS FINALLY OVER! THE HYPE SERVICE HAS LEFT THE STATION!", null));
 
 bot.startBot();
 
